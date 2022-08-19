072538fdbad0771a7aff99cd39e91cf0bbb64e6a
pengxu19890115@gmail.com <pengxu19890115@gmail.com>
2022-08-18 20:26:23 -0700

    add css styles

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,14 +1,17 @@
 import {useEffect, useState} from "react";
 
 
-export const Card = ({imageURL, id, updateScore, displayRandom, reset, hasReset}) => {
+export const Card = ({imageURL, id, updateScore, displayRandom, reset, hasReset, company, model}) => {
     const idNumber = `card${id}`
+
     const [selection, setSelection] = useState(false)
 
+    // callback function to update the "selection" state
     const updateSelection = () => {
         setSelection(a => !a)
     }
 
+    // call back function to reset state
     const resetState = () => {
         if (reset === true) {
             setSelection(false)
@@ -16,19 +19,22 @@ export const Card = ({imageURL, id, updateScore, displayRandom, reset, hasReset}
         }
     }
 
+    // callback functions to call parent functions
     useEffect(()=> {
         updateScore(selection)
         displayRandom()
         resetState()
     }, [selection, reset])
 
-    console.log('card')
     return (
-        <div className="carItem" id={idNumber}>
+        <div className="carItem" id={idNumber} onClick={updateSelection}>
+            <div className="carInfo">
+                <p className="model">{model}</p>
+                <p className="company">{company}</p>
+            </div>
             <img
                 src={imageURL}
                 className="car"
-                onClick={updateSelection}
             />
         </div>
     )
--- src/components/CardList.js
+++ src/components/CardList.js
@@ -11,6 +11,7 @@ export const CardList = () => {
 
     const [reset, setReset] = useState(false)
 
+    // update the score after selection
     const updateScore = childData => {
         if (childData) {
             setScore(a => a + 1)
@@ -23,12 +24,19 @@ export const CardList = () => {
         }
     }
 
+    // reset the "reset" state
     const hasReset = () => {
         setReset(false)
     }
 
+    // display the Card component
     const displayCardList = arr => {
         const cardList = arr.map((image, key) => {
+            const start = image.lastIndexOf('/')
+            const middle = image.indexOf('_')
+            const end = image.indexOf('.')
+            const company = image.slice(start + 1, middle)
+            const model = image.slice(middle + 1, end)
             return (
                 <Card
                     imageURL={image}
@@ -38,12 +46,15 @@ export const CardList = () => {
                     displayRandom={displayRandom}
                     reset={reset}
                     hasReset={hasReset}
+                    company={company}
+                    model={model}
                 />
             )
         })
         return cardList
     }
 
+    // change order value of card to display the Card component in random order
     const displayRandom = () => {
         const cardList = document.querySelectorAll(".carItem")
         cardList.forEach (card => {
@@ -51,14 +62,21 @@ export const CardList = () => {
         })
     }
 
-    console.log('card list')
     return (
         <div className="container">
             <div className="header">
-                <h1>Memory Card</h1>
                 <div className="heading">
-                    <h2>Score: {score} </h2>
-                    <h2>Best Score: {bestScore} </h2>
+                    <div className="gameHeading">
+                        <h2 className="heading1">Forza</h2>
+                        <div className="heading2">
+                            <h2 >Horizon</h2>
+                        </div>
+                    </div>
+                    <h1 className="headingGame">Memory Card</h1>
+                </div>
+                <div className="score">
+                    <h4>Score: {score} </h4>
+                    <h4>Best Score: {bestScore} </h4>
                 </div>
             </div>
             <div className="cardList">
